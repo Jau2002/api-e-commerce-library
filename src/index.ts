6726b7517b7bf4bdbadd70faa091599d14d76e24
Jau2002 <idamendjaume@gmail.com>
2023-03-10 16:38:07 -0500

    build: configuration repo

--- src/index.ts
+++ src/index.ts
@@ -16,10 +16,13 @@
 // /______________________/ /
 // \______________________\/
 
+import * as dotenv from 'dotenv';
 import app from './middlewares/app';
+
+dotenv.config({ path: '.env' });
 
 const PORT: number = process.env.PORT != null ? parseInt(process.env.PORT) : 0;
 
-app.listen(PORT, 'localhost', function () {
+app.listen(PORT, 'localhost', function (): void {
 	console.log(`http://localhost:${this.address().port}`);
 });
